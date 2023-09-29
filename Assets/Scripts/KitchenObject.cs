8f9883501eb1dadfd92dae24f7754a6394736cbc
Luna Lunapiena <lunacodes@gmail.com>
2023-09-29 16:04:04 -0400

    Add KitchenObject parent, and transfer object to other counter capability

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -3,7 +3,33 @@ using UnityEngine;
 public class KitchenObject : MonoBehaviour {
 	[SerializeField] private KitchenObjectSO kitchenObjectSO;
 
+	private ClearCounter clearCounter;
+
 	public KitchenObjectSO GetKitchenObjectSO() {
 		return kitchenObjectSO;
+	}
+
+	public void SetClearCounter(ClearCounter clearCounter) {
+		if (this.clearCounter != null) {
+			// Previous clear counter
+			this.clearCounter.ClearKitchenObject();
+		}
+
+		// New clear counter
+		if (clearCounter.HasKitchenObject()) {
+			Debug.LogError("Counter already has a KitchenObject!");
+		} else {
+			this.clearCounter = clearCounter;
+			clearCounter.SetKitchenObject(this);
+		}
+
+		//this.clearCounter = clearCounter;
+		//clearCounter.SetKitchenObject(this);
+
+		transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+		transform.localPosition = Vector3.zero;
+	}
+	public ClearCounter GetClearCounter() {
+		return clearCounter;
 	}
 }
