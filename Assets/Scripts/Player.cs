20daf44642715db272ab27bd0bff424c57bde0d3
Luna Lunapiena <lunacodes@gmail.com>
2023-10-03 21:09:24 -0400

    Add base ContainerCounter

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent {
 	public static Player Instance { get; private set; }
 	public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
 	public class OnSelectedCounterChangedEventArgs : EventArgs {
-		public ClearCounter selectedCounter;
+		public BaseCounter selectedCounter;
 	}
 
 	[SerializeField] private float moveSpeed = 7f;
@@ -15,7 +15,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent {
 
 	private bool isWalking;
 	private Vector3 lastInteractDir;
-	private ClearCounter selectedCounter;
+	private BaseCounter selectedCounter;
 	private KitchenObject kitchenObject;
 
 	private void Awake() {
@@ -55,10 +55,10 @@ public class Player : MonoBehaviour, IKitchenObjectParent {
 
 		float interactDistance = 2f;
 		if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask)) {
-			if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
+			if (raycastHit.transform.TryGetComponent(out BaseCounter baseCounter)) {
 				// Has ClearCounter
-				if (clearCounter != selectedCounter) {
-					SetSelectedCounter(clearCounter);
+				if (baseCounter != selectedCounter) {
+					SetSelectedCounter(baseCounter);
 				}
 			} else {
 				SetSelectedCounter(null);
@@ -114,7 +114,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent {
 		transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
 	}
 
-	private void SetSelectedCounter(ClearCounter selectedCounter) {
+	private void SetSelectedCounter(BaseCounter selectedCounter) {
 		this.selectedCounter = selectedCounter;
 
 		OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs {
